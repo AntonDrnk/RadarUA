6a658014ae8e68f7741ef51cd4f111ed9b9d9945
AntonDrnk <73669343+AntonDrnk@users.noreply.github.com>
2025-06-08 22:34:01 +0300

    Update app.js

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // --- PWA: РЕГИСТРАЦИЯ SERVICE WORKER И WAKE LOCK ---
     if ('serviceWorker' in navigator) {
         window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js').then(reg => console.log('Service worker registered.', reg)).catch(err => console.log('Service worker registration failed: ', err));
+            navigator.serviceWorker.register('service-worker.js').then(reg => console.log('Service worker registered.', reg)).catch(err => console.log('Service worker registration failed: ', err));
         });
     }
 
